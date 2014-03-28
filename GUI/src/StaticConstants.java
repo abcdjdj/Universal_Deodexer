4d9df939bb5b6992c04001da40dbf18c1ddd7274
Madhav Kanbur <abcdjdj@gmail.com>
2014-03-28 11:18:32 +0530

    Add author and api values

--- GUI/src/StaticConstants.java
+++ GUI/src/StaticConstants.java
@@ -1,8 +1,14 @@
 import java.awt.Color;
 
-
 public class StaticConstants
 {
 	public static final String TITLE = "Universal Deodex V5";
-	public static final Color BACKGROUND = new Color(22,132,219);
+	public static final Color BACKGROUND = new Color(22, 132, 219);
+	public static final String AUTHOR = "abcdjdj";
+	public static final String[] api_versions = {
+			"Default","4.4", "4.3", "4.2", "4.1", "4.0.3, 4.0.4",
+			"4.0, 4.0.1, 4.0.2", "3.2", "3.1.x", "3.0.x", "2.3.4, 2.3.3",
+			"2.3.2, 2.3.1, 2.3", "2.2.x", "2.1.x", "2.0.1", "2.0", "1.6",
+			"1.5", "1.1", "1.0"
+	};
 }
