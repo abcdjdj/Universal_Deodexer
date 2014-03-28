3723025400c3be676c75df771d0e8f58feb3a295
Madhav Kanbur <abcdjdj@gmail.com>
2014-03-28 10:53:57 +0530

    Add StatiConstants class

---
+++ GUI/src/StaticConstants.java
@@ -0,0 +1,8 @@
+import java.awt.Color;
+
+
+public class StaticConstants
+{
+	public static final String TITLE = "Universal Deodex V5";
+	public static final Color BACKGROUND = new Color(22,132,219);
+}
