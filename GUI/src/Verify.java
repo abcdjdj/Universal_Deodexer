609554429c321c0000e6eb2a66329e31b02a1c54
Madhav Kanbur <abcdjdj@gmail.com>
2014-04-07 22:44:01 +0530

    Updated GPL License headers

--- GUI/src/Verify.java
+++ GUI/src/Verify.java
@@ -1,3 +1,30 @@
+/**
+Universal Deodexer
+Copyright (C) 2014 Madhav Kanbur
+
+This file is part of Universal Deodexer.
+
+Universal Deodexer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Universal Deodexer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Universal Deodexer. If not, see <http://www.gnu.org/licenses/>.*/
+
+/**
+ * This class contains the functions for verifying if
+ * a apk/jar has been deodexed successfully.
+ * 
+ * @Author - Madhav Kanbur
+ * @Version - V5
+ */
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
